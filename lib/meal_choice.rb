2f56ee5eb82aee794fb7b8911e728780baa19b61
Beth Schofield <bethmschofield@gmail.com>
2019-10-24 22:26:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,3 @@ def meal_choice(veg1, veg2, protein = "parsnip")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   "A plate of #{protein} with #{veg1} and #{veg2}."
 end
-
-meal_choice("carrots", "parsnips", "seitan")
-meal_choice("carrots", "parsnips")
-meal_choice("carrots", "salmon")
